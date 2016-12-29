3efed125476a6f79445e3fe0148b5d41a2ceb549
Fred Lin <gasolin@mozilla.com>
2016-12-29 10:03:43 +0800

    update scripts

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 cd build
 git init
 git add -A
-git commit -m "Site updated: $(date + "%D %H-%M")"
+git commit -m "Site updated: $(date)"
 
-git push -u https://${GH_TOKEN}@github.com/gasolin/gasolin.github.io.git HEAD:gh-pages --force
+git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:gh-pages --force
