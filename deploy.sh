2c3e97bbd98072f209e4c3d9200cc0764511b368
Fred Lin <gasolin@mozilla.com>
2016-12-29 10:21:56 +0800

    move to source branch

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git init
 git add -A
 git commit -m "Site updated: $(date)"
 
-git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:gh-pages --force
+git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:master --force
