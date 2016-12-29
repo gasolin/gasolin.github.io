c7e751c17b5a0dad6aefb9119ba423212b8eb572
Fred Lin <gasolin@mozilla.com>
2016-12-29 14:23:30 +0800

    redirect output to null

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ git init
 git add -A
 git commit -m "Site updated: $(date)"
 
-echo "push to github"
-git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:master --force --quiet
+echo "push to github..."
+# remove git log by redirect log to null output
+git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:master --force &> /dev/null
 echo "done"
