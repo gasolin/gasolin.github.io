6b5cb6ae78fa90e74cdb4140f2df7eaf71a0a1cc
gasolin via Travis CI <gasolin@gmail.com>
2016-12-31 07:19:35 +0800

    push params in .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd build
+cd $FOLDER_TO_UPLOAD
 git init
 git add -A
-git commit -m "Site updated: $(date)"
+git commit -m "Site updated: $(TZ=:$TIMEZONE date)"
 
 echo "push to github..."
-# remove git log by redirect log to null output
-git push -u https://$GH_TOKEN@github.com/gasolin/gasolin.github.io.git HEAD:master --force &> /dev/null
+# remove git log by redirect log to /dev/null output
+git push -u https://$GH_TOKEN@github.com/$ACCOUNT/$REPO.git HEAD:$BRANCH --force &> /dev/null
 echo "done"
