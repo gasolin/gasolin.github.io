caa58d5615df5524b45cb2b36e4ef88ece3a9170
gasolin <gasolin@gmail.com>
2017-01-03 10:13:44 +0800

    add host info

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="row">
       <div className="col-lg-12">
         <br/>
-        ©2009~2017 by <a href="http://www.gasolin.idv.tw">gasolin</a>
+        ©2009~2017 by <a href="http://www.gasolin.idv.tw">gasolin</a>, hosted on <a href="https://github.com/gasolin/gasolin.github.io">github</a>
       </div>
     </div>
   );
