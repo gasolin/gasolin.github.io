175ac7e86688cb436bf3d03a6770a65e392fcb8e
Fred Lin <gasolin@mozilla.com>
2017-02-16 15:33:31 +0800

    move present data to presents-data.js

---
+++ src/components/pages/personal/presents-data.js
@@ -0,0 +1,228 @@
+const data = [
+  {
+    year: 2017,
+    presents: [
+    ],
+  },
+  {
+    year: 2016,
+    presents: [
+      {
+        title: '聊天機器人，不加咖啡',
+        date: '2016/8/21',
+        place: 'Coscup',
+        link: 'http://blog.gasolin.idv.tw/2016/08/blog-post.html',
+        time: '20min',
+      },
+      {
+        title: '好孩子的第一次挑戰 WebVR 就快快樂樂上手',
+        date: '2016/5/19',
+        place: 'workshop at MozTw',
+        link: 'http://moztw.kktix.cc/events/91d7bca0',
+        time: '30min',
+      },
+    ],
+  },
+  {
+    year: 2015,
+    presents: [
+      {
+        title: 'Javascript,征服世界是可能的嗎？ (Javascript, world domanation)',
+        date: '2015/5/19',
+        place: 'Modern Web',
+        link: 'http://www.slideshare.net/gasolin/javascript-48212668',
+        time: '30min',
+      },
+    ],
+  },
+  {
+    year: 2014,
+    presents: [
+      {
+        title: 'Firefox OS - How Large Open Source Project works',
+        date: '2014/7/19',
+        place: 'COSCUP',
+        link: 'http://www.slideshare.net/gasolin/firefox-os-how-large-open-source-project-works',
+        time: '30min',
+      },
+      {
+        title: 'All about Web Component',
+        date: '2014/5/7',
+        place: 'GDG Taipei',
+        link: '',
+        time: '30min',
+      },
+    ],
+  },
+  {
+    year: 2013,
+    presents: [
+      {
+        title: 'Capable Web: Chrome Apps & Firefox WebApp',
+        date: '2013/12/18',
+        place: 'GDG Taipei',
+        link: 'http://www.slideshare.net/gasolin/chrome-apps-and-firefox-webapp',
+        time: '30min',
+      },
+      {
+        title: 'Developing Firefox OS',
+        date: '2013/11/13',
+        place: 'NCTU CSIE',
+        link: 'http://www.slideshare.net/gasolin/develop-firefox-os1-28348187',
+        time: '2hr',
+      },
+      {
+        title: 'FirefoxOS Overview & Contribution',
+        date: '2013/10/28',
+        place: 'NCU CSIE',
+        link: '',
+        time: '3hr',
+      },
+      {
+        title: 'Steps to contribute to firefox os (gaia)',
+        date: '2013/7/27',
+        place: 'COSCUP',
+        link: 'http://www.slideshare.net/gasolin/steps-to-contribute-to-firefox-os-2',
+        time: '30min',
+      },
+      {
+        title: 'Hands on offline web app',
+        date: '2013/7/20',
+        place: 'COSCUP lecture',
+        link: 'http://registrano.com/events/coscup-2013-hands-on-gasolin',
+        time: '3hr',
+      },
+      {
+        title: 'Make web as webapp',
+        date: '2013/5/8',
+        place: 'GTUG Taipei',
+        link: 'http://www.slideshare.net/gasolin/make-web-as-webapp',
+        time: '1hr',
+      },
+      {
+        title: 'FirefoxOS Overview',
+        date: '2013/3/27',
+        place: 'NCKU CSIE',
+        link: 'http://www.slideshare.net/gasolin/firefox-os-overview-17792569',
+        time: '3hr',
+      },
+    ],
+  },
+  {
+    year: 2012,
+    presents: [
+      {
+        title: 'Youtube for developers',
+        date: '2012/10/25',
+        place: 'GTUG Taipei',
+        link: 'http://www.youtube.com/watch?v=hlu_TeBRpQA',
+        time: '30min',
+      },
+      {
+        title: 'Does everybody can program?',
+        date: '2012/8/19',
+        place: 'COSCUP',
+        link: 'http://www.youtube.com/watch?v=HEW39KXn0a4',
+        time: '25min',
+      },
+      {
+        title: 'Developer can make beautiful web - Introduction of JQuery Mobile/Twitter Bootstrap',
+        date: '2012/6/6',
+        place: 'GTUG Taipei',
+        link: 'http://www.youtube.com/watch?v=Xd2KiDunRxc',
+        time: '1hr',
+      },
+      {
+        title: 'Android UI Design Guide & Tips',
+        date: '2012/2/15',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=KnPKLuz6qpQ',
+        time: '30min',
+      },
+    ],
+  },
+  {
+    year: 2011,
+    presents: [
+      {
+        title: 'zhpy lightening talk',
+        date: '2011/12/3',
+        place: 'PyCon China',
+        link: 'http://www.youtube.com/watch?v=pQqs-8kyq88',
+        time: '5min',
+      },
+      {
+        title: 'gFolder demo',
+        date: '2011/12/2',
+        place: 'Taipei Android Developer Lab',
+        link: '',
+        time: '5min',
+      },
+      {
+        title: 'Android Icecream Sandwich(ICS) sharing',
+        date: '2011/11/9',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=PoKxf1xCDn0&feature=feedu',
+        time: '30min',
+      },
+      {
+        title: 'Android Continue Integration with Jenkins',
+        date: '2011/8/20',
+        place: 'COSCUP',
+        link: 'http://www.youtube.com/watch?v=vVwGuXuma-k',
+        time: '25min',
+      },
+      {
+        title: 'Introduction of AppInventor',
+        date: '2011/8/17',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=Ujd_wJBAd1k&feature=channel_video_title',
+        time: '3omin',
+      },
+      {
+        title: 'Play framework on Google App Engine',
+        date: '2011/7/20',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=Z0EYjejXcnY&feature=channel_video_title',
+        time: '1hr',
+      },
+      {
+        title: 'Share 3 Google I/O admob & analytics related speech',
+        date: '2011/7/6',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=fYgmsBjLnfE&feature=channel_video_title',
+        time: '1hr',
+      },
+      {
+        title: 'Google I/O Recap of Android',
+        date: '2011/5/28',
+        place: 'Engadget',
+        link: '',
+        time: '30min',
+      },
+      {
+        title: 'Beyond Android',
+        date: '2011/5/24',
+        place: 'III',
+        link: '',
+        time: '3hr',
+      },
+      {
+        title: 'Aside for Google I/O Extended Keynote',
+        date: '2011/5/11',
+        place: 'Taipei GTUG',
+        link: '',
+        time: '1hr',
+      },
+      {
+        title: 'Aside for Google Movie Day',
+        date: '2011/3/30',
+        place: 'Taipei GTUG',
+        link: 'http://www.youtube.com/watch?v=1wRlJ5KTAwE&feature=channel_video_title',
+        time: '1hr',
+      },
+    ],
+  },
+];
+
+export default data;
