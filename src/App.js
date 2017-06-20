dc91b0615e849d7ba4432f8dc566b94e3c62db8a
gasolin <gasolin@gmail.com>
2017-06-20 15:22:14 +0800

    add routeer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
 import Elevation from 'react-mdc-web/lib/Elevation';
 
 // import Header from './components/header';
