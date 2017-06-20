64fc3ee10aaf3d1354b85c7e8ca262b71ae73d6a
Fred Lin <gasolin@gmail.com>
2017-06-20 23:53:11 +0800

    use browserRouter again

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'material-components-web/dist/material-components-web.min.css';
 import App from './App';
 
