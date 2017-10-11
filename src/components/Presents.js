5740bba2d8eefcd2fb8ed40361c129161a6cc417
Fred Lin <gasolin@gmail.com>
2017-10-11 20:34:36 +0800

    add key

--- src/components/Presents.js
+++ src/components/Presents.js
@@ -7,10 +7,10 @@ const Presents = props => (<Grid>
   <Cell col={12}>
     <h2>Lectures/Presentations</h2>
   </Cell>
-  {data.map(yearly => (<Cell col={6}>
-     <h3 id="{yearly.year}">{yearly.year}</h3>
+  {data.map(yearly => (<Cell col={6} key={yearly.year}>
+     <h3 id={yearly.year}>{yearly.year}</h3>
      <ul>
-       {yearly.presents.map(item => <PresentsItem item={item}/>)}
+       {yearly.presents.map((item, idx) => <PresentsItem item={item} key={idx}/>)}
      </ul>
   </Cell>))}
 </Grid>);
